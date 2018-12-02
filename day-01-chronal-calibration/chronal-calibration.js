c5897adbbe7b2ab256e9a315711bc1d26a3129ac
Jeremy Hahn <jeremyhahn@Jeremys-MacBook-Pro.local>
2018-12-01 21:51:49 -0500

    Not efficient but it works

--- day-01-chronal-calibration/chronal-calibration.js
+++ day-01-chronal-calibration/chronal-calibration.js
@@ -1,5 +1,4 @@
-const chronalCalibration = (input) => {
-    console.log("Input = ", input);
+const chronalCalibration = (input) => { 
     const parsedInput = input.split('\n').map((x) => x.trim()).map( (x) => parseInt(x));
     
     return frequency = parsedInput.reduce((accumulator, currentValue) => {
--- day-01-chronal-calibration/chronal-calibration2.js
+++ day-01-chronal-calibration/chronal-calibration2.js
@@ -1,5 +1,32 @@
 const chronalCalibration = (input) => {
+    const parsedInput = 
+            input.split('\n')
+            .map((x) => x.trim())
+            .map((x) => parseInt(x));
+    
+    let iterator = 0;
+    let iterations = 1;
+    let foundDupFrequency = false;
+    let potentialDuplicate = 0;
+    const frequencySet = new Set([0]);
 
+    while(!foundDupFrequency) {
+        potentialDuplicate += parsedInput[iterator];
+        frequencySet.add(potentialDuplicate);
+        if(frequencySet.size !== iterations + 1) {
+            foundDupFrequency = true;
+        }
+        if(iterator === parsedInput.length -1) {
+            iterator = 0;
+        } else {
+            iterator++;
+        }
+        iterations++;
+        console.log("potential dup = ", potentialDuplicate);
+    }
+
+    console.log("potential dup to return = ", potentialDuplicate);
+    return potentialDuplicate;
 }
 
 module.exports = chronalCalibration;

3e79f3f3db2b225a093fabf355f5f73748c4baf6
Jeremy Hahn <Jeremy_Hahn@homedepot.com>
2018-12-06 14:56:29 -0500

    Formated and sorted

--- day-04-repose-record/repose-record.js
+++ day-04-repose-record/repose-record.js
@@ -1,5 +1,16 @@
-const reposeRecord = (input) => {
+var moment = require('moment');
+var _ = require('lodash');
 
-}
+const reposeRecord = input => {
+  const parsedInput = input.split("\n")
+  .map(x => x.replace("]", "|")
+  .trim())
+  .map(x => x.split("|")
+  .map(x => x.replace("[", "")));
+  
+  let sortedByDate = _.sortBy(parsedInput, function(o) { return new moment(o[0]); });
+
+  console.log("split input = ", splitInput);
+};
 
 module.exports = reposeRecord;

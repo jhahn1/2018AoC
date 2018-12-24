efef9ae9f38539ce010ac89508fb9dc0d55b05eb
Jeremy Hahn <jeremyhahn@Jeremys-MacBook-Pro.local>
2018-12-24 11:59:13 -0500

    Day 8 WIP'

--- day-08-memory-manuever/memory-manuever.js
+++ day-08-memory-manuever/memory-manuever.js
@@ -1,6 +1,20 @@
+var _ = require("lodash");
+
 const memoryManuever = (input) => {
-    let splitInput = input.split(" ");
-    console.log("nonsense");
+    let splitInput = input.split(" ").map((x) => x.trim()).map( (x) => parseInt(x));
+    let metadataList = [];
+    while(splitInput.length > 0) {
+        const children = splitInput[0];
+        const metaDataCount = splitInput[1];
+        _.pullAt(splitInput, [0, 1]);
+
+        for(let i = 0; i<metaDataCount; i++) {
+            let iterator = (children === 0) ? 0: splitInput.length -1;
+            metadataList.push(splitInput[iterator]);
+            _.pullAt(splitInput, iterator);
+        }
+    }
+    return _.sum(metadataList);
 };
 
 module.exports = memoryManuever;
